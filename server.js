7adff964a3ae1e12d1155934c1ef067cddc28286
Choih0401 <danielch0401@naver.com>
2020-04-16 17:47:23 +0900

    fix bug

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(morgan('combined'))
 
 app.use(express.static(__dirname + '/'));
 
-app.use('/',
-  function (req, res, next) {
-    res.sendFile(__dirname +  '/index.html');
-  }
-);
-
 app.all('*',
   function (req, res, next) {
     res.status(404).send('<h1> 404 Error </h1>');
+  }
+);
+
+app.use('/',
+  function (req, res, next) {
+    res.sendFile(__dirname +  '/index.html');
   }
 );
 
